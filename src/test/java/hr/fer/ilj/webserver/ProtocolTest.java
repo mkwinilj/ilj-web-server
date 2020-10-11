b7a0ae4d527ecf4692f40fbfcd59d5a1e91624df
MK Win ILJ <mario.kusek.win@gmail.com>
2020-10-11 17:42:54 +0200

    Sre]en prvi test

--- src/test/java/hr/fer/ilj/webserver/ProtocolTest.java
+++ src/test/java/hr/fer/ilj/webserver/ProtocolTest.java
@@ -2,14 +2,20 @@ package hr.fer.ilj.webserver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
 import org.junit.jupiter.api.Test;
 
 class ProtocolTest {
 
   @Test
-  void rootResponse() {
-    // TODO fix test
-    String protocolResult = "";
+  void rootResponse() throws IOException {
+    Protocol p = new Protocol();
+    BufferedReader inputBuffer = new BufferedReader(new StringReader("GET / HTTP 1.1\n"));
+    String protocolResult = p.processInput(inputBuffer);
     assertThat(protocolResult).isEqualTo("default");
   }
 }
+
